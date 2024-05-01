9517eeadc9a1bf33c2872b6fc013352f3d300b56
Neradovskikh Vladimir <skih.vladimir@gmail.com>
2024-05-01 20:04:04 +0300

    [feature] Изменил конфиг storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,9 @@
 import type {StorybookConfig} from "@storybook/react-webpack5";
 import * as path from "path";
+import postcssMixins from 'postcss-mixins';
+import autoprefixer from 'autoprefixer';
+import postcssImport from 'postcss-import';
+import postcssNested from 'postcss-nested';
 
 const config: StorybookConfig = {
     stories: ["../src/**/__stories__/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -11,11 +15,30 @@ const config: StorybookConfig = {
         "@chromatic-com/storybook",
         "@storybook/addon-interactions",
         {
-            name: "@storybook/addon-postcss",
+            name: '@storybook/addon-styling-webpack',
             options: {
-                postcssLoaderOptions: {
-                    implementation: require('postcss')
-                }
+                rules: [
+                    {
+                        test: /\.css$/,
+                        use: [
+                            'style-loader',
+                            'css-loader',
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    postcssOptions: {
+                                        plugins: () => [
+                                            postcssMixins,
+                                            autoprefixer,
+                                            postcssImport,
+                                            postcssNested,
+                                        ],
+                                    },
+                                }
+                            },
+                        ],
+                    },
+                ]
             }
         }
     ],
@@ -27,6 +50,19 @@ const config: StorybookConfig = {
         autodocs: "tag",
     },
     webpackFinal: async (config) => {
+        config.module.rules = [
+            ...config.module.rules,
+            {
+                test: /.*\.(ttf|eot|woff|woff2|gif|png|jpe?g)$/i,
+                type: 'asset/resource',
+            },
+        ];
+
+        console.log({
+            name: 'webpackFinal',
+            value: config.module.rules
+        });
+
         config.resolve.alias = {
             ...config.resolve.alias,
             '##': path.resolve(__dirname, "../src"),
