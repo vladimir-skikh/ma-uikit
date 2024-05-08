76a9de74f9b2a5b4b0012b6d46ffb4a927cdbec8
Neradovskikh Vladimir <skih.vladimir@gmail.com>
2024-05-08 15:47:03 +0300

    [feature] Доработал отображение истории для компонента Badge

--- src/components/Badge/__stories__/Badge.stories.ts
+++ src/components/Badge/__stories__/Badge.stories.ts
@@ -1,26 +1,82 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Badge as BadgeComponent } from '../Badge';
+import {Badge as BadgeComponent, badgeForms, BadgeProps, badgeSizes, badgeVariants, badgeViews} from '../Badge';
+import {ComponentWithAs} from "../../../utils";
 
 const meta = {
     title: 'Components/Badge',
     component: BadgeComponent,
-    tags: ['autodocs'],
     parameters: {
         layout: 'centered'
     },
-} satisfies Meta<typeof BadgeComponent>;
+} satisfies Meta<ComponentWithAs<BadgeProps>>;
 
 export default meta;
-type Story = StoryObj<typeof BadgeComponent>;
+type Story = StoryObj<ComponentWithAs<BadgeProps>>;
 
 export const Badge: Story = {
     args: {
+        label: 'Статус',
         size: 's',
         view: 'neutral',
         variant: 'primary',
-        label: 'Статус',
-        onlyDot: false,
-        form: 'default',
-        dot: true,
+        form: 'brick',
+        dot: false,
+    },
+    argTypes: {
+        label: {
+            control: 'text',
+            description: 'Текст лейбла',
+            table: {
+                category: 'Основные параметры',
+                type: {summary: 'string | number'}
+            },
+        },
+        size: {
+            control: 'select',
+            description: 'Размер',
+            options: badgeSizes as unknown as Array<string>,
+            table: {
+                category: 'Основные параметры',
+                defaultValue: {summary: 's'},
+                type: {summary: badgeSizes.join(' | ')}
+            },
+        },
+        view: {
+            control: 'select',
+            description: 'Цвет',
+            options: badgeViews as unknown as Array<string>,
+            table: {
+                category: 'Основные параметры',
+                defaultValue: {summary: 'neutral'},
+                type: {summary: badgeViews.join(' | ')}
+            },
+        },
+        variant: {
+            control: 'select',
+            description: 'Вариант',
+            options: badgeVariants as unknown as Array<string>,
+            table: {
+                category: 'Основные параметры',
+                defaultValue: {summary: 'primary'},
+                type: {summary: badgeVariants.join(' | ')}
+            },
+        },
+        form: {
+            control: 'select',
+            description: 'Форма',
+            options: badgeForms as unknown as Array<string>,
+            table: {
+                category: 'Основные параметры',
+                defaultValue: {summary: 'brick'},
+                type: {summary: badgeForms.join(' | ')}
+            },
+        },
+        dot: {
+            control: 'boolean',
+            description: 'Точка в бейдже',
+            table: {
+                category: 'Основные параметры',
+            },
+        },
     }
 };
